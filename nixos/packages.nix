e33ac8130f9d126f8ce44aaedc2c86c0faa88984
Malofeev Ivan <i.malofeev@expasoft.tech>
2025-03-18 14:53:40 +0700

    feat(nixos): some packages

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -20,6 +20,16 @@
     # For ags
     brightnessctl
     swww
+
+    # For work
+    shotcut # video editor
+    openvpn
+    yt-dlp # download from youtube
+    anydesk # remote desktop
+    libreoffice # documents
+    qgroundcontrol # air fly control
+    sublime4 # text editor
+    android-tools # adb
   ];
 
   fonts.packages = with pkgs; [
