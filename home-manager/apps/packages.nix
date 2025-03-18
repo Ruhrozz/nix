14c6df882852c1fb4f660723b8c5d5ed13634d3f
Malofeev Ivan <i.malofeev@expasoft.tech>
2025-03-18 14:53:57 +0700

    fix: remove unsupported packages

--- home-manager/apps/packages.nix
+++ home-manager/apps/packages.nix
@@ -4,8 +4,6 @@
     unzip
     dust # `du -h -d 1` replacement
     duf # `df -h` replacement
-    pre-commit # can be pip installed, tired from pip installing =)
-    uv # pip and pipenv better analog
     ripgrep # for nvim plugin (telescope)
     shellcheck # for nvim plugin (bashls)
   ];
