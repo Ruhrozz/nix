64539cc973744806575ea4c1b60de6ea7eef7fb8
Malofeev Ivan <i.malofeev@expasoft.tech>
2025-05-07 13:14:35 +0700

    feat: home-manager packages

--- home-manager/apps/packages.nix
+++ home-manager/apps/packages.nix
@@ -8,5 +8,9 @@
     ripgrep # for nvim plugin (telescope)
     shellcheck # for nvim plugin (bashls)
     ffmpeg # working with images and videos in CLI
+    clang # build packages
+    unzip # archivator
+    zip # archivator
+    p7zip # archivator
   ];
 }
